7ad48a5ba1cc705b7c77447cf46143fde5e68bd4
Rahul Chand <chandrahuls2212@gmail.com>
2026-02-03 16:27:44 +0530

    add booking function

--- HMS.c
+++ HMS.c
@@ -2,8 +2,6 @@
 
 /*------------------------------------function declarations----------------------------------------*/
 void header();
-void features();
-void input();
 void roombooking();
 void checkin();
 void checkout();
@@ -28,63 +26,92 @@ void header(){
     }
     printf("\n\n");
 
-    //call features
-    features();
-}
-
-/*-----------------------------------------print all features on screen-------------------------------------------------------------*/
-void features(){
-printf("1.Room Booking\n");
+    /*----------------------------------print all features on screen----------------------------*/
+    printf("1.Room Booking\n");
 printf("2.Check-in\n");
 printf("3.Check-out\n");
 printf("4.Customer Details\n");
 printf("5.Billing\n");
 printf("6.Exit");
 printf("\n\n");
-int i;
+
 for(i=0;i<=50;i++){
     printf("--");
 }
 
-    //call input;
-    input();
-}
-
-/*-----------------------------------------------take input----------------------------------------------------------*/
-void input(){
-int user_enter;
+printf("\n");
+/*-------------------------------------take input-----------------------------------------*/
+    int user_enter;
 printf("select option : ");
 scanf("%d",&user_enter);
+getchar(); //clear buffer
 
 switch(user_enter){
     case 1:
     roombooking();
     break;
 
-    case 2:
-    checkin();
-    break;
+    // case 2:
+    // checkin();
+    // break;
 
-    case 3:
-    checkout();
-    break;
+    // case 3:
+    // checkout();
+    // break;
 
-    case 4:
-    customer_details();
-    break;
+    // case 4:
+    // customer_details();
+    // break;
 
-    case 5:
-    billing();
-    break;
+    // case 5:
+    // billing();
+    // break;
 
-    case 6:
-    exit_program();
-    break;
+    // case 6:
+    // exit_program();
+    // break;
 
     default:
     printf("select vaild option\n");
 }
 }
+
+/*----------------------------------------structure of booking-----------------------------------------------------*/
+struct customer {
+    char name[30];
+    int room;
+    int days;
+    float bill;
+}c;
+
+/*----------------------------------------room booking--------------------------------------------------------------*/
+void roombooking() {
+    float charge = 1000;   // per day charge
+
+    printf("\n------ ROOM BOOKING ------\n");
+
+    printf("Enter Customer Name: ");
+    gets(c.name);
+    
+    printf("Enter Room Number: ");
+    scanf("%d", &c.room);
+   
+
+    printf("Enter Number of Days: ");
+    scanf("%d", &c.days);
+
+    c.bill = c.days * charge;
+
+    printf("\nRoom Booked Successfully!\n");
+    printf("----------------------------\n");
+    printf("Customer Name : %s\n", c.name);
+    printf("Room Number   : %d\n", c.room);
+    printf("Days Stayed   : %d\n", c.days);
+    printf("Total Bill    : %.2f\n", c.bill);
+    printf("----------------------------\n");
+}
+
+
 /*-------------------------------------------MAIN FUNCTION------------------------------------------------------------*/
 
 int main(){
